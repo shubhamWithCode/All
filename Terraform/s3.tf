5ef1aaf3e557d13b7d303953ea952f933183b78a
khanmohsin40 <151317788+khanmohsin40@users.noreply.github.com>
2024-02-23 15:45:01 +0530

    Update s3.tf

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -5,14 +5,15 @@ provider "aws" {
 # s3 bucker
 resource "aws_s3_bucket" "terraform-bucket" {
     bucket = "our-terraform-tfstate-file-bucket-shubham"
-    tags = {
-        Name = "first-bucket"
-        Env = "Dev"
-        Owner = "Shubham"
-        bucket = "mainBucket"
-    }
+}
+resource "aws_s3_bucket_acl""terraform-bucket-acl"{
+bucket = aws_s3_bucket.terrafrom-bucket.id
+acl="private"
+}
+
   
 }
+
 # versioning
 resource "aws_s3_bucket_versioning" "my-bucket-versioning" {
   bucket = aws_s3_bucket.terraform-bucket.id
